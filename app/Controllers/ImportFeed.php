1dff86c39bc44143216847170ca57f10e4a2f93b
Raoul Dzoukou <rd@atrocore.com>
2023-07-11 16:34:23 +0100

    .

--- app/Controllers/ImportFeed.php
+++ app/Controllers/ImportFeed.php
@@ -24,6 +24,9 @@ namespace Import\Controllers;
 
 use Espo\Core\Exceptions\BadRequest;
 use Espo\Core\Exceptions\Forbidden;
+use Espo\Core\Exceptions\NotFound;
+use Espo\Core\FilePathBuilder;
+use Espo\Core\Utils\Log;
 
 class ImportFeed extends \Espo\Core\Templates\Controllers\Base
 {
@@ -67,5 +70,106 @@ class ImportFeed extends \Espo\Core\Templates\Controllers\Base
         $attachmentId = property_exists($data, 'attachmentId') ? (string)$data->attachmentId : '';
 
         return $this->getRecordService()->runImport((string)$data->importFeedId, $attachmentId);
+    }
+
+    public function actionCreateFromExport($params, $data, $request)
+    {
+        if (!$request->isPost() || !property_exists($data, 'exportFeedId')) {
+            throw new BadRequest();
+        }
+
+        if (!$this->getAcl()->check($this->name, 'create')) {
+            throw new Forbidden();
+        }
+
+        $exportFeed = $this->getEntityManager()->getEntity('ExportFeed', $data->exportFeedId);
+
+        if (empty($exportFeed)) {
+            throw new NotFound();
+        }
+
+        $sourceFields = [];
+        foreach ($exportFeed->configuratorItems as $exportConfiguratorItem) {
+            $sourceFields[] = $exportConfiguratorItem->column;
+        }
+
+        $attachment = new \stdClass();
+        $attachment->name = $exportFeed->name . '(From Export)';
+        $attachment->description = $exportFeed->description;
+//        $attachment->code = $exportFeed->code;
+        $attachment->isActive = $exportFeed->isActive;
+        $attachment->type = 'simple';
+        $attachment->fileDataAction = 'update';
+        $attachment->sourceFields = $sourceFields;
+        $importFeed = $this->getRecordService()->createEntity($attachment);
+
+        foreach ($exportFeed->configuratorItems as $configuratorItem) {
+            $attachment = new \stdClass();
+            $attachment->importFeedId = $importFeed->id;
+            $attachment->name = $configuratorItem->name;
+            $attachment->column = [$configuratorItem->column];
+            $attachment->type = $configuratorItem->type;
+            $attachment->scope = $configuratorItem->scope;
+            $attachment->locale = $configuratorItem->locale;
+            $attachment->attributeId = $configuratorItem->attributeId;
+            $attachment->channelId = $configuratorItem->channelId;
+            $attachment->sortOrder = $configuratorItem->sortOrder;
+            $attachment->importBy = $configuratorItem->exportBy;
+
+            if ($configuratorItem->name === 'id') {
+                $attachment->entityIdentifier = true;
+            }
+
+            $this->getRecordService("ImportConfiguratorItem")->createEntity($attachment);
+        }
+
+        return ["id" => $importFeed->id];
+    }
+
+    public function actionEasyCatalog($params, $data, $request)
+    {
+        if (!$request->isPost() || !property_exists($data, 'code') || !property_exists($data, 'json')) {
+            throw new BadRequest();
+        }
+
+        $importFeed = $this->getEntityManager()->getRepository('ImportFeed')->where(['code' => $data->code])->findOne();
+        if (empty($importFeed)) {
+            throw new NotFound();
+        }
+
+        $repository = $this->getEntityManager()->getRepository('Attachment');
+        $attachment = $repository->get();
+        $attachment->set('name', 'easy-catalog.json');
+        $attachment->set('role', 'Import');
+        $attachment->set('storage', 'UploadDir');
+        $attachment->set('type', 'application/json');
+        $attachment->set('storageFilePath', $this->createPath());
+        $fileName = $repository->getFilePath($attachment);
+
+        $this->createDir($fileName);
+        file_put_contents($fileName, json_encode($data->json));
+        $attachment->set('size', \filesize($fileName));
+
+        $this->getEntityManager()->saveEntity($attachment);
+
+        $this->getRecordService()->runImport($importFeed->id, $attachment->id);
+        return true;
+    }
+
+    protected function createDir(string $fileName): void
+    {
+        $parts = explode('/', $fileName);
+        array_pop($parts);
+        $dir = implode('/', $parts);
+
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777, true);
+            sleep(1);
+        }
+    }
+
+    protected function createPath(): string
+    {
+        return $this->getContainer()->get('filePathBuilder')->createPath(FilePathBuilder::UPLOAD);
     }
 }
