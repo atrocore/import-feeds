46257f196a093b5edcbf55561227b3392cf1872c
Roman Ratsun <rr@atrocore.com>
2023-11-02 13:40:27 +0000

    .

--- app/Services/ImportFeed.php
+++ app/Services/ImportFeed.php
@@ -553,13 +553,11 @@ class ImportFeed extends Base
             $attachment->channelId = $configuratorItem->channelId;
             $attachment->sortOrder = $configuratorItem->sortOrder;
             $attachment->importBy = $configuratorItem->exportBy;
-            if (!empty($configuratorItem->attributeValue)) {
+            if (property_exists($configuratorItem, 'attributeValue') && !empty($configuratorItem->attributeValue)) {
                 $value = $configuratorItem->attributeValue;
-
                 if (!in_array($value, ['value', 'valueFrom', 'valueTo', 'valueUnit'])) {
                     $value = 'value';
                 }
-
                 $attachment->attributeValue = $value;
             }
 
