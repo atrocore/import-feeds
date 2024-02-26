4f022fb16076657805566a59f748d936aa3f8db0
Roman Ratsun <rr@atrocore.com>
2024-02-26 10:52:06 +0000

    .

--- app/Services/ImportConfiguratorItem.php
+++ app/Services/ImportConfiguratorItem.php
@@ -31,7 +31,6 @@ class ImportConfiguratorItem extends Base
             'default',
             'type',
             'attributeId',
-            'scope',
             'locale',
             'sortOrder',
             'foreignColumn',
