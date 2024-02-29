d5859fdd3caf601a472b6f7232b17ae7f5afd964
Kevin Teguia <kt@atrocore.com>
2024-02-29 09:24:42 +0100

    add support for postgres

--- app/Migrations/V1Dot5Dot61.php
+++ app/Migrations/V1Dot5Dot61.php
@@ -17,31 +17,45 @@ class V1Dot5Dot61 extends Base
 {
     public function up(): void
     {
-       $this->getConnection()
+        $qb = $this->getConnection()
             ->createQueryBuilder()
             ->update('import_configurator_item')
             ->set('name',':newName')
-            ->where("import_feed_id IN (SELECT id FROM import_feed WHERE  JSON_CONTAINS(data, :entity, '$.feedFields.entity') = 1 )")
-            ->andWhere('name=:name')
+            ->where('name=:name')
             ->setParameter('newName','extensibleEnums')
-            ->setParameter('name','extensibleEnum')
-            ->setParameter('entity','"ExtensibleEnumOption"')
-           ->executeQuery();
+            ->setParameter('name','extensibleEnum');
 
+        if($this->isPgSQL()){
+            $qb->andWhere("import_feed_id IN (SELECT id FROM import_feed WHERE  data::jsonb @> :entity::jsonb )")
+                ->setParameter('entity','{"feedFields": {"entity": "ExtensibleEnumOption"}}');
+        }else {
+            $qb->andWhere("import_feed_id IN (SELECT id FROM import_feed WHERE  
+                                    JSON_CONTAINS(data, :entity, '$.feedFields.entity') = 1 )")
+                ->setParameter('entity','"ExtensibleEnumOption"');
+        }
 
+        $qb->executeQuery();
     }
 
     public function down(): void
     {
-        $this->getConnection()
+        $qb = $this->getConnection()
             ->createQueryBuilder()
             ->update('import_configurator_item')
             ->set('name',':newName')
-            ->where("import_feed_id IN (SELECT id FROM import_feed WHERE  JSON_CONTAINS(data, :entity, '$.feedFields.entity') = 1 )")
-            ->andWhere('name=:name')
+            ->where('name=:name')
             ->setParameter('newName','extensibleEnum')
-            ->setParameter('name','extensibleEnums')
-            ->setParameter('entity','"ExtensibleEnumOption"')
-            ->executeQuery();
+            ->setParameter('name','extensibleEnums');
+
+        if($this->isPgSQL()){
+            $qb->andWhere("import_feed_id IN (SELECT id FROM import_feed WHERE  data::jsonb @> :entity::jsonb )")
+                ->setParameter('entity','{"feedFields": {"entity": "ExtensibleEnumOption"}}');
+        }else {
+            $qb->andWhere("import_feed_id IN (SELECT id FROM import_feed 
+                                        WHERE  JSON_CONTAINS(data, :entity, '$.feedFields.entity') = 1 )")
+                ->setParameter('entity','"ExtensibleEnumOption"');
+        }
+
+        $qb->executeQuery();
     }
 }
